ff7c3780b0fbabb1797d37493ce4f67291b6ec72
Mirwais Akrami <m7akrami770@gmail.com>
2023-10-02 02:10:50 -0400

    Feat: Library app

---
+++ src/main/java/com/example/library/entities/Author.java
@@ -0,0 +1,27 @@
+package com.example.library.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "author")
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "phone_no")
+    private String phoneNo;
+}
+
+
